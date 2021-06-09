368a5da51e0a5dfd57ab29881032e72ffe6d6fb6
Vasili Kavaleu <v.kavaleu@naveksoft.com>
2021-06-09 11:04:47 +0300

    Init

---
+++ todo_app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text, DateTime
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+from core.db import Base
+
+
+class Todo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(Text)
+    important = Column(Boolean)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="todo")
+
+
